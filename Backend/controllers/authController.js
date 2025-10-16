83eeb99e3e75e4570f2473240e06b3d3c59e88c4
azeem251 <azeemabdul13696@gmail.com>
2025-10-16 16:42:32 +0530

    Update authController.js

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -199,7 +199,7 @@ export const googleCallback = async (req, res) => {
   res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'Strict',
+    sameSite: 'None',
     maxAge: 7 * 24 * 60 * 60 * 1000,// 7 days
     //  domain: '192.168.1.34'
   });

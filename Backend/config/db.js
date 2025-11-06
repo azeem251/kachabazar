371dd7931d803e751022fd2e84693d447ef586bc
azeem251 <azeemabdul13696@gmail.com>
2025-11-06 17:42:25 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -24,7 +24,7 @@ const connectDB = async () => {
   }
 
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MongoDB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
